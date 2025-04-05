86a9625378236bce7aeedbdb59db7d58528a8e5f
raichi0205 <raichi.q.0205@gmail.com>
2025-04-05 22:45:14 +0900

    [Add] プレイヤーとぶつかった時吹っ飛ぶように

--- KudGame2/Assets/Script/HumanObject.cs
+++ KudGame2/Assets/Script/HumanObject.cs
@@ -5,6 +5,10 @@ namespace Kud.MainGame
 {
     public class HumanObject : HitObject
     {
+        bool isHit = false;
+        Vector3 displayOverPos = Vector3.zero;
+        Vector3 displayOverNormal = Vector3.zero;
+        [SerializeField] float rotationAxis = 1.0f;
 
         public override void Initialize(int _col, float _speed)
         {
@@ -21,7 +25,35 @@ namespace Kud.MainGame
         // Update is called once per frame
         protected override void Update()
         {
-            base.Update();
+            if (isHit) 
+            {
+                //transform.localRotation.SetEulerRotation(0, 0, rotationAxis);       // Hack: 治す
+                transform.localEulerAngles += new Vector3(0, 0, rotationAxis);
+                transform.position += displayOverNormal * 10.0f * Time.deltaTime;
+                Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+                if ((transform.position.x < displayOverPos.x && displayOverNormal.x < 0) || (transform.position.x > displayOverPos.x && displayOverNormal.x > 0))
+                {
+                    isHit = false;
+                    gameObject.SetActive(false);
+                    GameManager.Instance.UnActiveObject(objectType);
+                    transform.localRotation = Quaternion.identity;
+                }
+            }
+            else
+            {
+                base.Update();
+            }
+        }
+
+        protected override void OnHitPlayer(Player _player)
+        {
+            base.OnHitPlayer(_player);
+            Vector2 scale = Camera.main.WorldToScreenPoint(transform.localScale);                       // オブジェクトのスクリーンサイズ
+            float width = transform.position.x < 0 ? 0 - scale.x : Camera.main.pixelWidth + scale.x;    // 飛ばす左右方向
+            displayOverPos = Camera.main.ScreenToWorldPoint(new Vector3(width, 0, 0));                  // 飛ばす先のワールド座標
+            displayOverNormal = (displayOverPos - _player.transform.position).normalized;               // 飛ばす方向の単位ベクトル
+            displayOverNormal = new Vector3(displayOverNormal.x, Mathf.Abs(displayOverNormal.y));       // 飛ばす方向の単位ベクトルのy軸反転
+            isHit = true;
         }
     }
 }
