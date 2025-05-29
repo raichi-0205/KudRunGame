89dd6ede129c89a9eace624b73d76fdf3d558d63
raichi-0205 <raichi.q.0205@gmail.com>
2025-05-29 11:39:00 +0900

    [Add] NamedArrayDrawerがなかったために属性がうまく機能していなかったので追加

---
+++ KudGame2/Assets/Script/NamedArrayDrawer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+#if UNITY_EDITOR
+namespace Kud.Editor
+{
+    [CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+    public class NamedArrayDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            string[] names = ((NamedArrayAttribute)attribute).names;
+            int pos = int.Parse(property.propertyPath.Split('[', ']').Where(c => !string.IsNullOrEmpty(c)).Last());
+            if (pos < names.Length)
+            {
+                label.text = names[pos];
+            }
+            EditorGUI.PropertyField(rect, property, new GUIContent(names[pos]), includeChildren: true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, includeChildren: true);
+        }
+    }
+}
+#endif
